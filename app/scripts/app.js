2deb2f41c3e025bdbc755723cfe09be23d049577
Sebastian Menel <serbita@gmail.com>
2016-06-08 15:51:55 -0300

    Commit inicial

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cursoangularApp
+ * @description
+ * # cursoangularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cursoangularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'MainCtrl',
+        controllerAs: 'login'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cursoangularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cursoangularApp
+ */
+ angular.module('cursoangularApp')
+ .controller('MainCtrl', function ($scope) {
+   $scope.nombre="pera";
+
+   $scope.todos=[
+     {text:'Learn AngularJs',done:true},
+     {text:'Build an app',done:false}
+    ];
+
+    $scope.addTodo = function(){
+      $scope.todos.push({text:$scope.formTodoText,done:false})
+      $scope.formTodoText = '';
+    };
+
+    $scope.getTotalTodos = function(){
+      return $scope.todos.length;
+    };
+
+    $scope.clearCompleted = function(){
+      $scope.todos = _.filter($scope.todos, function(todo){
+        return !todo.done;
+      }
+    )};
+  })
+
+ .controller('LoginCtrl', function ($scope) {
+   $scope.nombre="pepe";
+ });
